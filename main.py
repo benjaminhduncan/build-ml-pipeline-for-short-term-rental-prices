7dd1d77ee2c6bcf1e21b5210bc2865b130f01613
Ben Duncan <benjamin.h.duncan@gmail.com>
2022-11-17 23:11:10 -0500

    add test_regression_model step

--- main.py
+++ main.py
@@ -118,13 +118,11 @@ def go(config: DictConfig):
 
         if "test_regression_model" in active_steps:
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/train_val_test_split",
+                f"{config['main']['components_repository']}/test_regression_model",
                 version="main",
                 parameters={
-                    "input": "clean_sample.csv:latest",
-                    "test_size": config["modeling"]["test_size"],
-                    "random_seed": config["modeling"]["random_seed"],
-                    "stratify_by": config["modeling"]["stratify_by"]
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
                 },
             )
 
